5fcf8f362af0e58d095df3a65eeb460cd2cd290f
MohammadSadeghGhobadian <ghobadian80@gmail.com>
2022-08-17 15:42:36 +0430

    cards are now sorted

--- src/main/java/ir/msghobadian/utils/Util.java
+++ src/main/java/ir/msghobadian/utils/Util.java
@@ -12,6 +12,7 @@ import java.security.SecureRandom;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.IntStream;
 
 import static ir.msghobadian.constants.Color.RED;
@@ -45,6 +46,27 @@ public class Util {
         return allFourHands;
     }
 
+    public static int cardSortingComparator(Card card1, Card card2) {
+        Type type1 =  card1.getType();
+        Type type2 =  card2.getType();
+        int number1 = card1.getNumber();
+        int number2 = card2.getNumber();
+        return type1 == type2 ? compareSameTypeCards(number1, number2) : sortTypes(type1, type2);
+    }
+
+    @SneakyThrows
+    private static int sortTypes(Type type1, Type type2) {
+        return type1.ordinal() - type2.ordinal();
+    }
+
+    public static int compareSameTypeCards(int number1, int number2) {
+        if(number1 == 1)
+            number1 += 13;
+        if(number2 == 1)
+            number2 += 13;
+        return number1 - number2;
+    }
+
     public static void sendSignal(Socket socket, String signal) {
         try {
             DataOutputStream out = new DataOutputStream(socket.getOutputStream());
@@ -63,7 +85,8 @@ public class Util {
             return in.readUTF();
         } catch (IOException e) {
             System.err.println("Game not sending any response");
-            Thread.sleep(1000);
+            AtomicInteger timeout = new AtomicInteger(5000);
+            Thread.sleep(timeout.getAndAdd(5000));
         } catch (NullPointerException n){
             System.err.println("Socket is null");
         }
@@ -91,4 +114,6 @@ public class Util {
             throw new RuntimeException(e);
         }
     }
+
+
 }
